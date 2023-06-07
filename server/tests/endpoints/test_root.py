e238f0f1625f4aa5032539fe6a3fc8bab82911bf
szerintedmi <szerintedmi@gmail.com>
2023-06-07 22:31:50 +0100

    catchup with tests for status response

--- server/tests/endpoints/test_root.py
+++ server/tests/endpoints/test_root.py
@@ -1,20 +1,34 @@
 import pytest
 from httpx import AsyncClient
+from deepdiff import DeepDiff
 
 
 @pytest.mark.anyio
 async def test_read_main(endpoint: AsyncClient, mock_db, mock_user):
+    expected_response = {
+        "version": "0.0.1",
+        "message": "prompton-api is running",
+        "dbstatus": {"status_code": 1, "status_message": "{'ok': 1.0}"},
+    }  # +github_sha, github_env present
+
     response = await endpoint.get("/")
     db_status = str(await mock_db.command("ping"))
 
     assert response.status_code == 200
-    assert response.json() == {
-        "version": "0.0.1",
-        "message": "prompton-api is running",
-        "dbstatus": {"status_code": 1, "status_message": "{'ok': 1.0}"},
-        "github_sha": None,
-        "github_env": None,
-    }
+    actual_response = response.json()
+
+    diff = DeepDiff(
+        expected_response,
+        actual_response,
+        ignore_order=True,
+        exclude_paths=["github_sha", "github_env"],
+    )
+
+    print(" *** actual  vs. expected diff: \n", diff.pretty())
+    assert diff == {}, "expected and actual response  should match"
+
+    assert "github_sha" in actual_response
+    assert "github_env" in actual_response
 
 
 # TODO: 404 tests
