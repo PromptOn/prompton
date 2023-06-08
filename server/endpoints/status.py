ba72eebb1fba464529056f2ebafe7a6013d695b1
szerintedmi <szerintedmi@gmail.com>
2023-06-08 10:33:59 +0100

    apiStatus internal naming

--- server/endpoints/status.py
+++ server/endpoints/status.py
@@ -2,27 +2,27 @@ from fastapi import APIRouter, Depends
 
 from server.core.settings import settings
 from server.core.database import get_db
-from server.schemas import underTheHood
+from server.schemas import apiStatus
 from server.core.database import get_db
 
 router = APIRouter()
 
 
 @router.get(
-    "/status", response_model=underTheHood.ApiStatusResponse, tags=["Server status"]
+    "/status", response_model=apiStatus.ApiStatusResponse, tags=["Server status"]
 )
-async def get_status(db=Depends(get_db)) -> underTheHood.ApiStatusResponse:
+async def get_status(db=Depends(get_db)) -> apiStatus.ApiStatusResponse:
     # TODO: proper db status check + handling exceptions including timeout.  move it to a health endpoint & figure what's to show on /
-    dbstatus: underTheHood.DBStatus
+    dbstatus: apiStatus.DBStatus
     if db is None:
-        dbstatus = underTheHood.DBStatus(status_code=0, status_message="not connected")
+        dbstatus = apiStatus.DBStatus(status_code=0, status_message="not connected")
     else:
-        dbstatus = underTheHood.DBStatus(
+        dbstatus = apiStatus.DBStatus(
             status_code=1,
             status_message=str(await db.command("ping")),
         )
 
-    status = underTheHood.ApiStatusResponse(
+    status = apiStatus.ApiStatusResponse(
         version="0.0.1",
         message="prompton-api is running",
         dbstatus=dbstatus,
