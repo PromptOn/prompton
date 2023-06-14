37a505e10cf055f861d891c659f74c132eea8f93
szerintedmi <szerintedmi@gmail.com>
2023-06-14 23:40:41 +0100

    add db index to inferences.end_user_id

--- server/scripts/db_init_indexes_and_first_user.py
+++ server/scripts/db_init_indexes_and_first_user.py
@@ -36,9 +36,10 @@ async def run():
     await db.inferences.create_index([("created_by_org_id", ASCENDING)])
 
     await db.promptVersions.create_index([("prompt_id", ASCENDING)])
-    await db.inferences.create_index([("prompt_id", ASCENDING)])
 
+    await db.inferences.create_index([("prompt_id", ASCENDING)])
     await db.inferences.create_index([("prompt_version_id", ASCENDING)])
+    await db.inferences.create_index([("end_user_id", ASCENDING)])
 
     print("Adding inital user: ", os.getenv("PROMPTON_USER_EMAIL"))
     org = {
