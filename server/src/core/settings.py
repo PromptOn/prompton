29a781ca9456469ab9e2b7613df2ae3199aaac73
szerintedmi <szerintedmi@gmail.com>
2023-06-27 20:26:57 +0100

    make secret env vars SecretStr

--- server/src/core/settings.py
+++ server/src/core/settings.py
@@ -2,13 +2,13 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    DATABASE_URL: SecretStr
     MONGO_DATABASE: str
 
     DEFAULT_OPENAI_REQUEST_TIMEOUT_SECONDS: float = 90
 
     JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-    JWT_SECRET_KEY: str
+    JWT_SECRET_KEY: SecretStr
     JWT_ALGORITHM: str = "HS256"
 
     GITHUB_ENV: str | None
